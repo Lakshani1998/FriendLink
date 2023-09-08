fa45f5456eac7a72d056cffeb49ff3ef2e1529b2
Lakshani Erandika <lakshaniudawela@gmail.com>
2023-09-08 16:05:11 +0530

    feed-share container structure created

--- src/components/Share/Share.jsx
+++ src/components/Share/Share.jsx
@@ -1,22 +1,38 @@
 import React from 'react'
 import "./share.css"
-import { PermMedia } from '@mui/icons-material'
+import { PermMedia,Label,Room,EmojiEmotions } from '@mui/icons-material'
 
 export default function Share() {
   return (
     <div className='shareContainer'>
       <div className="shareWrapper">
         <div className="shareTop">
-            {/* <img className='shareProfileImg' src="/assets/person/11.jpg" alt="" /> */}
+            <img className='shareProfileImg' src="/assets/person/11.jpg" alt="" />
             <input placeholder='What in your mind' className="shareInput" />
         </div>
         <hr className="shareHr" />
         <div className="shareBottom">
            <div className="shareOptions">
                 <div className="shareOption">
-                    <PermMedia className='shareIcon'/>
-                    <span className='shareOptionText'>Photo or Video</span>
+                    <PermMedia htmlColor='tomato' className='shareIcon'/>
+                    <span className='shareOptionText'>Photo</span>
                 </div>
+
+                <div className="shareOption">
+                    <Label htmlColor='Blue' className='shareIcon'/>
+                    <span className='shareOptionText'>Tag  </span>
+                </div>
+
+                <div className="shareOption">
+                    <Room htmlColor='green' className='shareIcon'/>
+                    <span className='shareOptionText'>Locations  </span>
+                </div>
+
+                <div className="shareOption">
+                    <EmojiEmotions htmlColor='goldenrod' className='shareIcon'/>
+                    <span className='shareOptionText'>Feelings  </span>
+                </div>
+
             </div> 
         </div>
       </div>
