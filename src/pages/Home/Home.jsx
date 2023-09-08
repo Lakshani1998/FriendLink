28d08062bd9b10e28ce5abe020065f9feaa31503
Lakshani Erandika <lakshaniudawela@gmail.com>
2023-09-08 11:50:42 +0530

    Adding feed, sidebar and rightbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Topbar from '../../components/Topbar/Topbar'
+import Sidebar from '../../components/Sidebar/Sidebar'
+import Feed from '../../components/Feed/Feed'
+import Rightbar from '../../components/Rightbar/Rightbar'
+
+
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Topbar/>
-    </div>
+      <div className="homeContainer">
+      <Sidebar/>
+      <Feed/>
+      <Rightbar/>
+      
+      </div>
+      
+      
+    </>
   )
 }
